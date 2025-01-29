2e1f1e22d4478b77810cbeb0380b8c924cc5217e
Alex Sh <shalex.work@gmail.com>
2025-01-29 22:55:32 +0200

    Add proto files support

--- server/source/main.cpp
+++ server/source/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "proto/helloworld.pb.h"
 
 int main() {
     std::cout << APP_NAME << " v" << APP_VERSION_MAJOR << "." << APP_VERSION_MINOR << "." << APP_VERSION_PATCH << APP_VERSION_DIRTY << std::endl;
